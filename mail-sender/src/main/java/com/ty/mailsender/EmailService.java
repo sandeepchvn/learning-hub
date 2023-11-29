1d8405ffc7920eb5a8ac31828267f68836467227
sandeep chavan <134943186+sandeepchvn@users.noreply.github.com>
2023-11-29 15:53:21 +0530

    Update EmailService.java

--- mail-sender/src/main/java/com/ty/mailsender/EmailService.java
+++ mail-sender/src/main/java/com/ty/mailsender/EmailService.java
@@ -22,7 +22,6 @@ public class EmailService {
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
-//        message.setFrom("sandeepchvn1999@gmail.com");
         javaMailSender.send(message);
     }
 }
