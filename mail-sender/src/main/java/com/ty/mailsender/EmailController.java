b1c7e85f03efa56f1140d726acc038ac75e53cc9
sandeepchavan24 <sandeepchvn1999@gmail.com>
2023-11-29 15:28:57 +0530

    mail sender code is added

---
+++ mail-sender/src/main/java/com/ty/mailsender/EmailController.java
@@ -0,0 +1,19 @@
+package com.ty.mailsender;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @GetMapping("/send-email")
+    public String sendEmail(@RequestParam String to, @RequestParam String subject, @RequestParam String text) {
+        emailService.sendEmail(to, subject, text);
+        return "Email sent successfully!";
+    }
+}
---
+++ mail-sender/src/main/java/com/ty/mailsender/EmailService.java
@@ -0,0 +1,22 @@
+package com.ty.mailsender;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+//        message.setFrom("sandeepchvn1999@gmail.com");
+        javaMailSender.send(message);
+    }
+}
